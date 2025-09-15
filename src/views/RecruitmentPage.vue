358e4be1337452fe7522b862f94036dc679668d2
VassiliProko <vassiligb12@gmail.com>
2025-09-15 13:38:37 +0800

    update recruitment page details

--- src/views/RecruitmentPage.vue
+++ src/views/RecruitmentPage.vue
@@ -15,7 +15,7 @@ import SceneAmpharos from '@/components/SceneAmpharos.vue';
 					<p>So you want to be part of MCSS...</p>
 					<p>If you've ever wanted to be a part of a loving and caring family 🏠, have plenty fun while planning exciting events 🤪, and make friends with people that have the drippiest swag 😎, then MCSS is the perfect place for you!</p>
 					<p>We have two recruitment periods that open depending on position availabilities: Fall and Winter.</p>
-					<p>Recruitment for the 2025-2026 AY is coming soon.</p>
+					<p>2025 Fall Recruitment has passed.</p>
 				</div>
 			</div>
 		</div>
@@ -24,15 +24,15 @@ import SceneAmpharos from '@/components/SceneAmpharos.vue';
 				<div class="text-wrapper">
 					<h2>Open Positions</h2>
 					<div class="positions">
-						<!-- <p>Stay tuned...</p> -->
-						<PositionCard v-for="position in positions" :key="position" :position="position"/>
+						<p>Stay tuned...</p>
+						<!-- <PositionCard v-for="position in positions" :key="position" :position="position"/> -->
 					</div>
 				</div>
-				<Btn label="Apply" variant="primary" href="https://forms.gle/HCNvXkEEZyMgb5zz6" target="_default"/>
+				<!-- <Btn label="Apply" variant="primary" href="https://forms.gle/HCNvXkEEZyMgb5zz6" target="_default"/>
 				<div class="text-wrapper">
 					<small>Application deadline: <strong>Tuesday, September 9th</strong></small>
 					<small><strong>Interviews:</strong> In-person interviews will be held the week after the deadline.</small>
-				</div>
+				</div> -->
 			</div>
 		</div>
 	</div>
