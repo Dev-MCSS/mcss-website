4ece9345dc7a254f3e70c47a79f674645d8674dc
VassiliProko <vassiligb12@gmail.com>
2026-02-03 11:40:00 +0800

    update sponsors

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -3,70 +3,110 @@ export const sponsors = {
         span: "Restaurants",
         items: [
             {
+                name: "Bistro Japonais Furusato",
+                image: "sponsors/bistro_japonais_furusato",
+                discount: "10% off, except nabemono and sushi",
+                address: "1265 Rue Beaubien E"
+            },
+            {
+                name: "Cafe Noya",
+                image: "sponsors/cafe_noya",
+                discount: "15% off",
+                address: "2053 Peel St"
+            },
+            {
+                name: "Canada Best",
+                image: "sponsors/canada_best",
+                discount: "15% off + 1 drink",
+                address: "997 Av. Ogilvy"
+            },
+            {
+                name: "Cup Montreal",
+                image: "sponsors/cup_montreal",
+                discount: "15% off",
+                address: "404 Av. Duluth E"
+            },
+            {
                 name: "DONDONYA",
                 image: "sponsors/dondonya",
                 discount: "10% off",
                 address: "1433A Bishop Street"
             },
             {
-                name: "Cuisine Formosa",
-                image: "sponsors/cuisine_formosa",
+                name: "Foofoo noodz",
+                image: "sponsors/foofoo_noodz",
                 discount: "10% off",
-                address: "2130 Rue Mackay"
+                address: "6565 Av. Somerled"
+            },
+            {
+                name: "Krapow",
+                image: "sponsors/krapow",
+                discount: "10% off",
+                address: "4449 Mentana St"
+            },
+            {
+                name: "Restaurant Pheonix Oriental",
+                image: "sponsors/phoenix_oriental",
+                discount: "10% off",
+                address: "7230 Bd Maurice-Duplessis"
+            },
+            {
+                name: "Restaurant Pinophyta",
+                image: "sponsors/pinophyta",
+                discount: "10% off",
+                address: "1186 Av. Union"
+            },
+            {
+                name: "Mai Thai Cuisine",
+                image: "sponsors/mai_thai_cuisine",
+                discount: "10% off",
+                address: "1743 R. Saint-Denis"
+            },
+            {
+                name: "Nos Thés (Ste-Cath/Guy)",
+                image: "sponsors/nos_thes",
+                discount: "10% off",
+                address: "1609 Saint-Catherine St W"
+            },
+            {
+                name: "Pho Anh",
+                image: "sponsors/pho_anh",
+                discount: "10% off",
+                address: "1765 R. Saint Denis"
+            },
+            {
+                name: "Ramen Isshin (all locations)",
+                image: "sponsors/ramen_isshin",
+                discount: "15% off",
+                addresses: [
+                    "1861 Rue Sainte-Catherine O",
+                    "1217 Mont-Royal Ave E",
+                    "3876 Wellington St"
+                ]
+            },
+            {
+                name: "Satu Lagi",
+                image: "sponsors/satu_lagi",
+                discount: "10% off",
+                address: "1361 Mont-Royal Ave E"
+            },
+            {
+                name: "Yen Cuisine Japonaise",
+                image: "sponsors/yen_cuisine_japonaise",
+                discount: "10% off",
+                address: "2157 Mackay St"
             },
             {
                 name: "Délicieux Mei Mei",
                 image: "sponsors/mei_mei",
-                discount: "10% off",
+                discount: "10% off card, 15% off cash",
                 address: "2188 Rue Ste-Catherine O"
-            },
-            {
-                name: "Dobe and Andy",
-                image: "sponsors/dobe_and_andy",
-                discount: "10% off",
-                address: "1083 Boulevard Saint-Laurent"
-            },
-            {
-                name: "Ganadara (McGill)",
-                image: "sponsors/ganadara",
-                discount: "10% off, 15% off for cash",
-                address: "2077 Blvd Robert-Bourassa"
             },
             {
                 name: "Le Restaurant Dumpling Shop",
                 image: "sponsors/dumpling_shop",
                 discount: "10% off",
                 address: "3575 Park Ave"
-            },
-            {
-                name: "LiuYiShou",
-                image: "sponsors/liuyishou",
-                discount: "10% off for AYCE fee, excludes soup base fee, Cash Only. Monday-Friday before 5PM. Free All You Can Drink",
-                address: "1224 Crescent St"
-            },
-            {
-                name: "Malatang ZhangLiang",
-                image: "sponsors/zhangliang",
-                discount: "10% off",
-                address: "30 Rue De la Gauchetiere O"
-            },
-            {
-                name: "Pho Tay Ho",
-                image: "sponsors/pho_tay_ho",
-                discount: "10% off",
-                address: "6414 Rue Saint-Denis"
-            },
-            {
-                name: "Raviolis Impressions (Ste-Catherine W)",
-                image: "sponsors/raviolis_impressions",
-                discount: "10% off",
-                address: "368 Rue Ste-Catherine W"
-            },
-            {
-                name: "Resto Seoul",
-                image: "sponsors/resto_seoul",
-                discount: "10% off",
-                address: "4287 Saint Denis St"
             },
             {
                 name: "Sansotei Ramen",
@@ -77,80 +117,108 @@ export const sponsors = {
                     "1453 Saint-Catherine St W"
                 ]
             },
-            {
-                name: "Thai Express (Parc)",
-                image: "sponsors/thai_express",
-                discount: "10% off",
-                address: "Galerie du Parc, 3550 Park Ave"
-            },
-            {
-                name: "Tsukuyomi (Mile end)",
-                image: "sponsors/tsukuyomi",
-                discount: "10% off",
-                address: "5207 Boulevard Saint-Laurent"
-            },
-            {
-                name: "Masa/DOI (Faubourg Mall)",
-                image: "sponsors/masa",
-                discount: "10% off",
-                address: "1616 Rue Ste-Catherine O"
-            },
-            {
-                name: "Ahi Poke",
-                image: "sponsors/ahi_poke",
-                discount: "10% off",
-                address: "820 Mont-Royal Ave E"
-                // add multiple addresses if needed
-            }
+            // ARCHIVE SPONSORS
+            // {
+            //     name: "Cuisine Formosa",
+            //     image: "sponsors/cuisine_formosa",
+            //     discount: "10% off",
+            //     address: "2130 Rue Mackay"
+            // },
+            // {
+            //     name: "Dobe and Andy",
+            //     image: "sponsors/dobe_and_andy",
+            //     discount: "10% off",
+            //     address: "1083 Boulevard Saint-Laurent"
+            // },
+            // {
+            //     name: "Ganadara (McGill)",
+            //     image: "sponsors/ganadara",
+            //     discount: "10% off, 15% off for cash",
+            //     address: "2077 Blvd Robert-Bourassa"
+            // },
+            // {
+            //     name: "LiuYiShou",
+            //     image: "sponsors/liuyishou",
+            //     discount: "10% off for AYCE fee, excludes soup base fee, Cash Only. Monday-Friday before 5PM. Free All You Can Drink",
+            //     address: "1224 Crescent St"
+            // },
+            // {
+            //     name: "Malatang ZhangLiang",
+            //     image: "sponsors/zhangliang",
+            //     discount: "10% off",
+            //     address: "30 Rue De la Gauchetiere O"
+            // },
+            // {
+            //     name: "Pho Tay Ho",
+            //     image: "sponsors/pho_tay_ho",
+            //     discount: "10% off",
+            //     address: "6414 Rue Saint-Denis"
+            // },
+            // {
+            //     name: "Raviolis Impressions (Ste-Catherine W)",
+            //     image: "sponsors/raviolis_impressions",
+            //     discount: "10% off",
+            //     address: "368 Rue Ste-Catherine W"
+            // },
+            // {
+            //     name: "Resto Seoul",
+            //     image: "sponsors/resto_seoul",
+            //     discount: "10% off",
+            //     address: "4287 Saint Denis St"
+            // },
+            // {
+            //     name: "Thai Express (Parc)",
+            //     image: "sponsors/thai_express",
+            //     discount: "10% off",
+            //     address: "Galerie du Parc, 3550 Park Ave"
+            // },
+            // {
+            //     name: "Tsukuyomi (Mile end)",
+            //     image: "sponsors/tsukuyomi",
+            //     discount: "10% off",
+            //     address: "5207 Boulevard Saint-Laurent"
+            // },
+            // {
+            //     name: "Masa/DOI (Faubourg Mall)",
+            //     image: "sponsors/masa",
+            //     discount: "10% off",
+            //     address: "1616 Rue Ste-Catherine O"
+            // },
+            // {
+            //     name: "Ahi Poke",
+            //     image: "sponsors/ahi_poke",
+            //     discount: "10% off",
+            //     address: "820 Mont-Royal Ave E"
+            //     // add multiple addresses if needed
+            // }
         ]
     },
     "Drinks & Desserts": {
         span: "Drinks & Desserts",
         items: [
             {
-                name: "Nos Thés (Ste-Cath/Guy)",
-                image: "sponsors/nos_thes",
-                discount: "10% off",
-                address: "1609 Saint-Catherine St W"
-            },
-            {
                 name: "Tsujiri",
                 image: "sponsors/tsujiri",
-                discount: "10% off",
+                discount: "10% off except retail products & whole cakes",
                 address: "1418 Rue Crescent"
             },
             {
-                name: "GongCha",
-                image: "sponsors/gongcha",
-                discount: "15% off (cash)",
-                addresses:  [
-                    "55 St Catherine St E, H2X 1K5",
-                    "9389 Boul. Leduc"
-                ]
+                name: "Charyu tea house",
+                image: "sponsors/charyu_tea_house",
+                discount: "10% off on tea bags",
+                address: "6872 Rue St-Hubert"
             },
             {
-                name: "OCHA (Ste-Cath W)",
-                image: "sponsors/ocha",
-                discount: "15% off",
-                address: "1651 Rue Ste-Catherine O"
+                name: "La Muse (Lincoln Ave)",
+                image: "sponsors/la_muse",
+                discount: "Free drinks during happy hour",
+                address: "1608 Av. Lincoln"
             },
             {
-                name: "Zoe Dessert",
-                image: "sponsors/zoe_dessert",
+                name: "Lakafe",
+                image: "sponsors/lakafe",
                 discount: "10% off",
-                address: "2160 St Mathieu St"
-            },
-            {
-                name: "Dessert Coréen Crofre",
-                image: "sponsors/crofre",
-                discount: "15% off in-person",
-                address: "5172 Queen Mary Rd"
-            },
-            {
-                name: "CoCo (Ste-Cath)",
-                image: "sponsors/coco",
-                discount: "10% off",
-                address: "1388 Rue Ste-Catherine W"
+                address: "2115 Boul. Saint-Laurent"
             },
             {
                 name: "Matcha Zanmai",
@@ -159,29 +227,107 @@ export const sponsors = {
                 address: "1428 Mackay St"
             },
             {
-                name: "Allo Daisy",
-                image: "sponsors/allo_daisy",
-                discount: "",
-                address: "912 Boul. de Maisonneuve E"
-            },
-            {
-                name: "DontYellAtMe",
-                image: "sponsors/dontyellatme",
+                name: "OCHA (Ste-Cath W)",
+                image: "sponsors/ocha",
                 discount: "15% off",
-                address: "100 Rue Saint-Antoine O"
+                address: "1651 Rue Ste-Catherine O"
             },
             {
-                name: "Salon Thé Chai (Parc)",
-                image: "sponsors/salon_the_chai",
+                name: "Oishi Bar à Matcha chain",
+                image: "sponsors/oishi_bar_matcha",
+                discount: "10% off foods and drinks",
+                address: "1289 Rue Beaubien E"
+            },
+            {
+                name: "Pâtisserie Amaïro",
+                image: "sponsors/patisserie_amairo",
+                discount: "10% off (1 cake + 1 drink)",
+                address: "3911 Rue Saint-Jacques"
+            },
+            {
+                name: "Pâtisserie Bao Bao Dim Sum",
+                image: "sponsors/patisserie_bao_bao_dim_sum",
                 discount: "10% off",
-                address: "3506 Park Ave"
+                address: "83 Rue De la Gauchetiere O"
             },
             {
-                name: "Chiuonthese",
-                image: "sponsors/chiuonthese",
-                discount: "10% off cake orders",
-                link: "https://www.instagram.com/chiuonthese/"
-            }
+                name: "Pâtisserie Bervig Inc.",
+                image: "sponsors/patisserie_bervig_inc",
+                discount: "10% off",
+                address: "33 R. Notre Dame O"
+            },
+            {
+                name: "San-O Brasserie",
+                image: "sponsors/san_o_brasserie",
+                discount: "10% off",
+                address: "1350 Rue Mazurette Unit 104"
+            },
+            {
+                name: "#Teashop",
+                image: "sponsors/teashop",
+                discount: "20% off",
+                address: "52C Rue De la Gauchetiere O"
+            },
+            {
+                name: "Thes Guru Store",
+                image: "sponsors/thes_guru_store",
+                discount: "10% off on loose-leaf teas",
+                address: "1289 Rue Beaubien E"
+            },
+            // ARCHIVE SPONSORS
+            // {
+            //     name: "GongCha",
+            //     image: "sponsors/gongcha",
+            //     discount: "15% off (cash)",
+            //     addresses:  [
+            //         "55 St Catherine St E, H2X 1K5",
+            //         "9389 Boul. Leduc"
+            //     ]
+            // },
+            
+            // {
+            //     name: "Zoe Dessert",
+            //     image: "sponsors/zoe_dessert",
+            //     discount: "10% off",
+            //     address: "2160 St Mathieu St"
+            // },
+            // {
+            //     name: "Dessert Coréen Crofre",
+            //     image: "sponsors/crofre",
+            //     discount: "15% off in-person",
+            //     address: "5172 Queen Mary Rd"
+            // },
+            // {
+            //     name: "CoCo (Ste-Cath)",
+            //     image: "sponsors/coco",
+            //     discount: "10% off",
+            //     address: "1388 Rue Ste-Catherine W"
+            // },
+            
+            // {
+            //     name: "Allo Daisy",
+            //     image: "sponsors/allo_daisy",
+            //     discount: "",
+            //     address: "912 Boul. de Maisonneuve E"
+            // },
+            // {
+            //     name: "DontYellAtMe",
+            //     image: "sponsors/dontyellatme",
+            //     discount: "15% off",
+            //     address: "100 Rue Saint-Antoine O"
+            // },
+            // {
+            //     name: "Salon Thé Chai (Parc)",
+            //     image: "sponsors/salon_the_chai",
+            //     discount: "10% off",
+            //     address: "3506 Park Ave"
+            // },
+            // {
+            //     name: "Chiuonthese",
+            //     image: "sponsors/chiuonthese",
+            //     discount: "10% off cake orders",
+            //     link: "https://www.instagram.com/chiuonthese/"
+            // }
         ]
     }
 };
